0c01807bdeb83b064a16a6fe276218a2fc51d2c3
sw1fft <bogdanow.w02@gmail.com>
2024-10-03 21:15:05 +0300

    Update

--- JdmMarketplace.API/DTO/Requests/RequestDTO.cs
+++ JdmMarketplace.API/DTO/Requests/RequestDTO.cs
@@ -5,11 +5,9 @@ namespace JdmMarketplace.Services.CatalogAPI.DTO.Requests
     public class RequestDTO
     {
         [Required]
-        [Range(5, 15)]
         public string? Name { get; set; }
 
-        [Range(10, 1000)]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [Required]
         public decimal Price { get; set; } = decimal.Zero;
